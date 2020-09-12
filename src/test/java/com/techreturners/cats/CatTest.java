f95be7bf5481dca388b21d868e718d24f840d88d
Arul-Amirtha <arul.arulnathan@gmail.com>
2020-09-12 16:41:41 +0100

    Initial class implementation

--- src/test/java/com/techreturners/cats/CatTest.java
+++ src/test/java/com/techreturners/cats/CatTest.java
@@ -7,12 +7,14 @@ import static org.junit.Assert.*;
 public class CatTest {
 
     @Test
+
     public void checkCatIsAwake() {
         Cat domesticCat = new DomesticCat();
         assertFalse("Cat should be awake by default", domesticCat.isAsleep());
     }
 
     @Test
+
     public void checkCatCanGoToSleep() {
         Cat domesticCat = new DomesticCat();
         domesticCat.goToSleep();
@@ -20,6 +22,7 @@ public class CatTest {
     }
 
     @Test
+
     public void checkCatCanWakep() {
         Cat domesticCat = new DomesticCat();
         domesticCat.goToSleep();
@@ -28,37 +31,42 @@ public class CatTest {
     }
 
     @Test
+
     public void checkCatSetting() {
         Cat domesticCat = new DomesticCat();
         assertEquals("domestic", domesticCat.getSetting());
     }
 
     @Test
+
     public void checkCatHeight() {
         Cat domesticCat = new DomesticCat();
         assertEquals(23, domesticCat.getAverageHeight());
     }
 
     @Test
+
     public void checkLionHeight() {
         Cat lionCat = new LionCat();
         assertEquals(1100, lionCat.getAverageHeight());
     }
 
-
     @Test
+
     public void feedTheLion() {
         Cat lionCat = new LionCat();
         assertEquals("Roar!!!!", lionCat.eat());
     }
 
     @Test
+
     public void feedTheCheetah() {
         Cat cheetahCat = new CheetahCat();
         assertEquals("Zzzzzzz", cheetahCat.eat());
     }
 
     @Test
+
     public void feedTheCat() {
         Cat domesticCat = new DomesticCat();
         assertEquals("Purrrrrrr", domesticCat.eat());
